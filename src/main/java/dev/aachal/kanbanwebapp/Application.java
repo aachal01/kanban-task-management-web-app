acc20a12483203167fe180bbff9d28520d7796f3
aachal01 <aachalmodak10@gmail.com>
2023-08-25 15:28:14 +0530

    Added HomePage ApI, added welcome message and about application using @ConfigurationProperties

--- src/main/java/dev/aachal/kanbanwebapp/Application.java
+++ src/main/java/dev/aachal/kanbanwebapp/Application.java
@@ -1,12 +1,15 @@
 package dev.aachal.kanbanwebapp;
 
+import dev.aachal.kanbanwebapp.config.ContentProperties;
 import dev.aachal.kanbanwebapp.model.Board;
 import dev.aachal.kanbanwebapp.repository.contentRepository.BoardRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+@EnableConfigurationProperties(ContentProperties.class)
 @SpringBootApplication
 public class Application {
 
